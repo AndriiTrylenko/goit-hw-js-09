8994dc9d33e4cc41222ab3dd1678957835b0895f
AndriiTrylenko <trilenkoandrey@gmail.com>
2023-02-09 13:41:44 +0200

    reset

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,30 @@
+
+const body = document.querySelector('body');
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+
+btnStart.addEventListener('click', onStartClick);
+btnStop.addEventListener('click', onStopClick);
+
+btnStop.setAttribute('disabled', '');
+const COLOR_DELAY = 1000;
+let intervalId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartClick() {
+    btnStart.setAttribute('disabled', '');
+    btnStop.removeAttribute('disabled', '');
+    body.style.backgroundColor = getRandomHexColor();
+    intervalId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, COLOR_DELAY);
+}
+
+function onStopClick() {
+    btnStop.setAttribute('disabled', '');
+    btnStart.removeAttribute('disabled', '');
+    clearInterval(intervalId);
+}
